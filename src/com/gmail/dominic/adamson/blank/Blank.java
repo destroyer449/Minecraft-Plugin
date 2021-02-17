fb640f00481a9ccf65411e40f3a4332a63fb5ced
Dominic Adamson <adamson.dom@gmail.com>
2021-02-17 09:23:36 -0700

    initial commit

---
+++ src/com/gmail/dominic/adamson/blank/Blank.java
@@ -0,0 +1,14 @@
+package com.gmail.dominic.adamson.blank;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Blank extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+}
